3ed22621e8b4b004d31c723eb24046702745c7f3
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-18 01:00:53 +0100

    fix first task again

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,8 @@ const { body, buttonStart, buttonStop } = {
   buttonStop: document.querySelector('button[data-stop]'),
 };
 
+let colorSwitcher;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
